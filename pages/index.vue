00db52000b9e4fcafc173209c7aab955bb73b1e1
Misha Gusev <majikx.prod0@gmail.com>
2025-03-09 12:41:20 +0500

    feat: add title for index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,16 @@
+<script lang="ts" setup>
+  useSeoMeta({
+    title: "Avion",
+    description: "To-do list.",
+  });
+</script>
+
 <template>
-  <LandingHeroSection />
-  <div class="flex flex-col gap-12">
-    <LazyLandingPreviewSection />
-    <LazyLandingFeaturesSection />
+  <div>
+    <LandingHeroSection />
+    <div class="flex flex-col gap-12">
+      <LazyLandingPreviewSection />
+      <LazyLandingFeaturesSection />
+    </div>
   </div>
 </template>
