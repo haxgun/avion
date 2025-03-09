53ce452a0302dcb4977d997c355a9114503172a7
Misha Gusev <majikx.prod0@gmail.com>
2025-03-09 12:41:40 +0500

    feat: add fetch priority for image in preview

--- components/Landing/PreviewSection.vue
+++ components/Landing/PreviewSection.vue
@@ -3,7 +3,12 @@
     <div
       class="container overflow-hidden rounded-[1.25rem] p-0 shadow-[0rem_0.25rem_0.75rem_rgba(12,20,66,0.02),0rem_1.875rem_5rem_rgba(12,20,66,0.08)] ring-1 ring-[#e0e2e4]"
     >
-      <NuxtImg src="/intro.webp" class="w-full rounded-[1.25rem]" alt="INTRO" />
+      <NuxtImg
+        src="/intro.webp"
+        class="w-full rounded-[1.25rem]"
+        alt="INTRO"
+        fetchpriority="high"
+      />
     </div>
   </div>
 </template>
