295af677946a42d9214644cf2db04d808762af5c
Misha Gusev <majikx.prod0@gmail.com>
2025-03-09 12:03:18 +0500

    feat: add icons in hero section

--- components/Landing/HeroSection.vue
+++ components/Landing/HeroSection.vue
@@ -1,8 +1,15 @@
+<script lang="ts" setup>
+  import { Play, Stars02 } from "untitledui-js/vue";
+</script>
+
 <template>
   <div class="flex h-[55dvh] flex-col items-center justify-center gap-8">
-    <span class="rounded-full bg-[#008fff] px-4 py-1 text-xs font-medium text-white">{{
-      $t("landing.hero.popup")
-    }}</span>
+    <span
+      class="flex flex-row items-center justify-center gap-2 rounded-full border border-white/10 bg-lightblue/70 px-4 py-1 text-xs font-medium text-white transition-all hover:bg-lightblue/80"
+    >
+      <Stars02 :size="12" color="#fff" />
+      {{ $t("landing.hero.popup") }}
+    </span>
     <h1 class="whitespace-pre-line text-center text-6xl font-black leading-[1]">
       {{ $t("landing.hero.title.first") }}
       <span class="text-[#008fff]">{{ $t("landing.hero.title.smart") }}</span>
@@ -12,7 +19,10 @@
       {{ $t("landing.hero.description") }}
     </h4>
     <div class="flex flex-row gap-3">
-      <UiButton>{{ $t("landing.hero.buttons.watch") }}</UiButton>
+      <UiButton>
+        <Play :size="16" />
+        {{ $t("landing.hero.buttons.watch") }}
+      </UiButton>
       <UiButton variant="blue"
         ><NuxtLink to="/registration">{{ $t("landing.hero.buttons.start") }}</NuxtLink></UiButton
       >
