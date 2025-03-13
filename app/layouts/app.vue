af269168ec7be4fee8fa1145df87897574a5a831
Misha Gusev <majikx.prod0@gmail.com>
2025-03-13 11:51:10 +0500

    chore: Rename Sidebar components and update imports

--- app/layouts/app.vue
+++ app/layouts/app.vue
@@ -1,11 +1,7 @@
-<script setup lang="ts">
-  import Sidebar from "@/components/Ui/Sidebar/Sidebar.vue";
-</script>
-
 <template>
   <div class="flex h-dvh w-full flex-col bg-[#F7F8FA] pl-[calc(22rem+8px)]">
     <LazyUiHighlights />
-    <Sidebar />
+    <UiSidebar />
     <div class="relative mx-auto my-0 flex min-h-full w-full max-w-2xl flex-col">
       <slot />
     </div>
