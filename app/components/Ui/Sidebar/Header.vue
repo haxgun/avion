e34049195e2b87dd52bb139aa523ebe1b2196075
Misha Gusev <majikx.prod0@gmail.com>
2025-03-15 07:35:29 +0500

    chore: Simplify Sidebar Header button component

--- app/components/Ui/Sidebar/Header.vue
+++ app/components/Ui/Sidebar/Header.vue
@@ -14,10 +14,7 @@
 <template>
   <div class="flex flex-row items-center justify-between px-4">
     <NuxtImg src="/logo.svg" width="73" height="20" fetchpriority="high" />
-    <button
-      @click="emit('toggle')"
-      class="flex h-8 w-8 items-center justify-center rounded-lg transition-colors hover:bg-gray-100"
-    >
+    <button @click="emit('toggle')">
       <AlignLeft01 class="text-gray-500 transition-colors hover:text-gray-700" :size="16" />
     </button>
   </div>
